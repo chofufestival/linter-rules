7e81f539abb7d896e71d339eacddb837b8386834
いーちゃん(e_chan1007) <echan041007@gmail.com>
2024-02-29 17:23:12 +0900

    fix: tseslint also tried to lint js files

--- eslint/ts.mjs
+++ eslint/ts.mjs
@@ -7,27 +7,33 @@ import tseslint from "typescript-eslint";
 ...compat.extends("plugin:@typescript-eslint/recommended"),
   ...compat.extends("plugin:@typescript-eslint/stylistic"),
   */
-export default tseslint.config({
-  extends: [...tseslint.configs.recommended, ...tseslint.configs.stylistic],
-  languageOptions: {
-    parserOptions: {
-      project: true
+export default tseslint.config(
+  {
+    extends: [...tseslint.configs.recommended, ...tseslint.configs.stylistic],
+    languageOptions: {
+      parserOptions: {
+        project: true
+      }
+    },
+    rules: {
+      "@typescript-eslint/no-non-null-assertion": "warn",
+      "@typescript-eslint/no-duplicate-enum-values": "off",
+      "@typescript-eslint/no-unsafe-declaration-merging": "off",
+      "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/ban-tslint-comment": "off",
+      "@typescript-eslint/class-literal-property-style": "off",
+      "@typescript-eslint/consistent-generic-constructors": "off",
+      "@typescript-eslint/consistent-indexed-object-style": "off",
+      "@typescript-eslint/consistent-type-assertions": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/no-confusing-non-null-assertion": "off",
+      "@typescript-eslint/prefer-for-of": "off",
+      "@typescript-eslint/prefer-function-type": "off",
+      "@typescript-eslint/require-await": "error"
     }
   },
-  rules: {
-    "@typescript-eslint/no-non-null-assertion": "warn",
-    "@typescript-eslint/no-duplicate-enum-values": "off",
-    "@typescript-eslint/no-unsafe-declaration-merging": "off",
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/ban-tslint-comment": "off",
-    "@typescript-eslint/class-literal-property-style": "off",
-    "@typescript-eslint/consistent-generic-constructors": "off",
-    "@typescript-eslint/consistent-indexed-object-style": "off",
-    "@typescript-eslint/consistent-type-assertions": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/no-confusing-non-null-assertion": "off",
-    "@typescript-eslint/prefer-for-of": "off",
-    "@typescript-eslint/prefer-function-type": "off",
-    "@typescript-eslint/require-await": "error"
+  {
+    files: ["**/*.js", "**/*.cjs", "**/*.mjs"],
+    extends: [tseslint.configs.disableTypeChecked]
   }
-});
+);
