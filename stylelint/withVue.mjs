cf2c457f0af3bac07dcfec95385e625353ce5d53
いーちゃん(e_chan1007) <echan041007@gmail.com>
2024-05-03 17:35:43 +0900

    chore: add vue config

---
+++ stylelint/withVue.mjs
@@ -0,0 +1,28 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: [
+    "stylelint-config-standard-scss",
+    "stylelint-config-recommended-vue/scss",
+    "stylelint-config-sass-guidelines",
+    "stylelint-config-recess-order",
+    "@stylistic/stylelint-config",
+    "stylelint-config-html/vue"
+  ],
+  plugins: [
+    "@double-great/stylelint-a11y",
+    "stylelint-declaration-block-no-ignored-properties",
+    "stylelint-value-no-unknown-custom-properties"
+  ],
+  rules: {
+    "a11y/font-size-is-readable": true,
+    "a11y/no-obsolete-attribute": true,
+    "a11y/no-obsolete-element": true,
+    "a11y/no-outline-none": true,
+    "a11y/selector-pseudo-class-focus": true,
+    "color-named": "never",
+    "declaration-property-value-no-unknown": true,
+    "plugin/declaration-block-no-ignored-properties": true,
+    "csstools/value-no-unknown-custom-properties": true
+  },
+  ignoreFiles: ["**/node_modules/**"]
+};
