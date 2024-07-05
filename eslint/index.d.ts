d9c9dfacbf82b4f2d7d66411929265caaf303281
いーちゃん(e-chan1007) <echan041007@gmail.com>
2024-07-05 15:51:13 +0900

    fix(eslint): 設定の型がおかしい

--- eslint/index.d.ts
+++ eslint/index.d.ts
@@ -1,6 +1,6 @@
 import type { Linter } from "eslint";
 
-type FlatConfig = Linter.FlatConfig;
+type FlatConfig = Linter.FlatConfig[];
 
 declare module "@chofufestival/linter-rules/eslint" {
   export = FlatConfig;
